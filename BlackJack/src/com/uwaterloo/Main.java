a474bc2416ee971651ef797c20dd2dd2c165b608
Wojtechnology <Wojtek Swiderski>
2014-08-05 13:26:32 -0400

    Creating of Main Files
    Created the Main and Screen files. Opens a window.

---
+++ BlackJack/src/com/uwaterloo/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.uwaterloo;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author wojtekswiderski
+ */
+public class Main {
+    
+    public static void main(String [] args){
+        Screen window = new Screen(800, 600);
+    }
+    
+}
